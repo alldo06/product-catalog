6c842ac203f59ea935e711764e425c9478d78fb4
alldo06 <aldovector06@gmail.com>
2024-04-05 22:04:32 +0700

    feat: add error pages and footer component

---
+++ error.vue
@@ -0,0 +1,28 @@
+<script setup>
+const error = useError()
+
+const handleError = () => {
+  clearError({
+    redirect: `/`
+  })
+}
+
+// eslint-disable-next-line no-console
+console.log(error.message)
+</script>
+
+<template>
+  <div>
+    <Header />
+    <div v-if="error" class="empty-state__container">
+      <h1 class="empty-state__title">404 Not Found</h1>
+      <p class="subtitle">Your visited page not found. You may go home page.</p>
+      <button
+        class="btn btn-lg btn-primary rounded leading-normal mt-6"
+        @click="handleError"
+      >
+        Back to home page
+      </button>
+    </div>
+  </div>
+</template>
